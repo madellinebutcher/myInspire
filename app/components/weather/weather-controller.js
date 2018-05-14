69d98a71f31e7d61f12bef6991ff720395561db2
Madelline Butcher <madellinebutcher@gmail.com>
2018-05-14 13:50:49 -0600

    updated

--- app/components/weather/weather-controller.js
+++ app/components/weather/weather-controller.js
@@ -2,12 +2,16 @@ function WeatherController(){
 	var wc = this;
     var weatherService = new WeatherService();
     
+    
+
     function drawWeather(weather){
         var fahrenheit = Math.floor((9/5) * (weather.main.temp - 273) + 32);
+        
         var template = ""
         template += `
         <div class="w-bg">
-        <h5 class="mx-2">${fahrenheit}°</h5>
+        <h5 class="mx-2">${fahrenheit}°F</h5>
+        <img class="w-img" src="http://openweathermap.org/img/w/${weather.weather[0].icon}.png"
         
         <h6 class="mx-2">${weather.name}</h6>
         </div>
